7ac55fa0ba37f2ee45dba7e1e9cbf3678e8d2a07
Rodrigo M <rodrigo.mercado.galaz@gmail.com>
2022-05-21 19:41:49 -0400

    Creación de proyecto

---
+++ src/main/java/com/init/operacion/quasar/OperacionQuasarApplication.java
@@ -0,0 +1,14 @@
+package com.init.operacion.quasar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperacionQuasarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperacionQuasarApplication.class, args);
+		//holi
+	}
+
+}
---
+++ src/test/java/com/init/operacion/quasar/OperacionQuasarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.init.operacion.quasar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperacionQuasarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
